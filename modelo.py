b739487d9fc3492fdfcf9846bccfd1aa23b55164
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-02-24 08:28:34 -0300

    feat: adiciona detalhes a classe playlist

--- modelo.py
+++ modelo.py
@@ -45,14 +45,61 @@ class Serie(Programa):
         return f'A série {self.nome} foi lançada em {self.ano} e tem {self.temporadas} temporadas'
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.__programas = programas
+
+    def __setitem__(self, key, value):
+        self.__programas[key] = value
+
+    def __getitem__(self, item):
+        return self.__programas[item]
+
+    def __len__(self):
+        return len(self.__programas)
+
+    def programa_esta_na_lista(self, progr_selecionado):
+        tipo_do_programa = 'A série' if type(progr_selecionado) == Serie else 'O filme'
+        nome_programa = progr_selecionado.nome
+        esta_na_lista = 'está' if progr_selecionado in self else 'não está'
+        playlist_nome = self.nome
+
+        return f'{tipo_do_programa} {nome_programa} {esta_na_lista} '\
+               f'dentro da playlist {playlist_nome}.'
+
+
 vingadores = Filme('Vingadores', 2018, 160)
-vingadores.dar_like()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+flash = Serie('Flash', 2017, 6)
+
+vingadores.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
+atlanta.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, tmep, demolidor]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+vingadores.nome = 'Vingadores: Ultimato'
+playlist_fim_de_semana[0] = vingadores
+
+print(f'Tamanho da playlist "{playlist_fim_de_semana.nome}": '
+      f'{len(playlist_fim_de_semana)} programas.\n')
+
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print('')
+
+print(playlist_fim_de_semana.programa_esta_na_lista(vingadores))
+
+print(playlist_fim_de_semana.programa_esta_na_lista(flash))
